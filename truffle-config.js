99f3544a1239e1f270bc88375564773f2d7ca363
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2021-07-24 01:39:30 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -59,7 +59,7 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
+    tronWeb: {
       provider: () => new HDWalletProvider(mnemonic, `https://api.trongrid.io`),
       network_id: 1,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
