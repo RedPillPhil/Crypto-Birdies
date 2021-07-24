9d6ea94d78013e72016f276ced931047b71fe5db
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2021-07-24 01:50:04 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -61,7 +61,7 @@ module.exports = {
     // NB: It's important to wrap the provider as a function.
     tronWeb: {
       provider: () => new HDWalletProvider(mnemonic, `https://api.tronstack.io`),
-      network_id: 1,       // Ropsten's id
+      network_id: 0,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
