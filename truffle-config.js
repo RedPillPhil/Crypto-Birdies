72b32263a55c8ad36b58dc8fb1db7edfddd68d93
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2021-07-24 15:15:36 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -61,7 +61,7 @@ module.exports = {
     // NB: It's important to wrap the provider as a function.
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`),
-      network_id: 2,       // Ropsten's id
+      network_id: 4,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
