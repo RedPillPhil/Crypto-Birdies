55f98b2e79578a0eb5bea543ba24ff17af324487
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2021-07-27 12:03:52 -0400

    Update blockchain.js

--- client/assets/js/blockchain.js
+++ client/assets/js/blockchain.js
@@ -4,7 +4,7 @@ ethereum.autoRefreshOnNetworkChange = true;
 var birdInstance;
 var marketInstance;
 var user;
-var access = false;
+var access = true;
 var birdAddress = "0x448f14E3209543ebba581f30E0f8f0570311423D"; //Ropsten: 0x70e2324ccf7a76e201dff26d4749ed1bb821c305
 var marketAddress = "0x7ef6293D4ebBE4baDC3B02A5232bFD44cE65b6fE"; // Ropsten: 0x78ad2f9c3924278692125a23ed05d4e5facfd97c
 
