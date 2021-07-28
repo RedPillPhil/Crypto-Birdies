7308a84c5931ed555d4858b555ca55cc9c6886d6
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2021-07-28 17:35:19 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -61,7 +61,7 @@ module.exports = {
     // NB: It's important to wrap the provider as a function.
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://api.shasta.trongrid.io`),
-      network_id: 3,       // Ropsten's id
+      network_id: 2,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
